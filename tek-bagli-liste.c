8158eb49fd9c310097e7df32d7008416ab4ccd9a
mehtayn <mehtayn@hotmail.com>
2015-02-25 22:45:48 +0200

    Update tek-bagli-liste.c

--- tek-bagli-liste.c
+++ tek-bagli-liste.c
@@ -1,119 +1,118 @@
 www.mehtayn.com
 
-#include "stdio.h"
-#include "conio.h"
-#include "stdlib.h"//free ve malloc fonksiyonlarımızın çalışması için gereken kütüphanelerimiz
-struct dugum //dugum adlı yeni bir yapı tanımlıyoruz.
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+struct node
 {
-	int veri;//dugum adlı yapımızın değişkenleri
-	dugum *sonraki;//dugum yapımızın bir sonraki veriyi göstermek amacıyla kullanacagımız pointer değişkenimiz
+	int data;
+	node *next;
 };
-dugum *basaekle(dugum *head) //yapımıza baştan ekleme yapılacaksa
+node *addhead(node *head) //basa ekle
 {
-	dugum *gecici=(dugum *)malloc(sizeof(dugum));//yeni bir eleman eklediğimiz için gecici adlı değişkenimize dinamik bellekte yer ayırıyoruz
-	gecici->veri=8;//gecici değişkenimizin verisine 8 değerini atadık.
-	gecici->sonraki=head; //gecici değişkenimizin bir sonraki elemanı head ı göstersin
-	head=gecici;//yeni head imizde gecici olsun. Böylelikle basa eklenmiş olucak
-	return head;// fonksiyonu cağırdıgımız yere head imizi döndürecek.
+	node *temp=new node();
+	temp->data=8;
+	temp->next=head;
+	head=temp;
+	return head;
 }
-dugum *sonaekle(dugum *head) //yapımıza sondan ekleme yapılacaksa
+node *addlast(node *head) // sona ekle
 {
-	dugum *gecici=(dugum *)malloc(sizeof(dugum));//yeni bir eleman eklediğimiz için gecici adlı değişkenimize dinamik bellekte yer ayırıyoruz
-	gecici-> veri=7;//gecici değişkenimizin verisine7 değerini atadık.
-	gecici->sonraki=NULL;//sona ekleme yaptıgımız için bu sefer gecici değişenimizin bir sonraki elemanını NULL yaptık.
-	dugum *gecici2=head;//yeni bir gecici2 değişkenini head i tanımladık.
-	while(gecici2->sonraki!=NULL)//while döngüsü ile head ten yani baştan sona kadar döndürme işlemi ile son elemanı bulucak
-								//Bunuda gecici2 değişkenimize atacak
-		gecici2=gecici2->sonraki;//NULL eşit olmayıncaya kadar bir sonrakine gittik.
-	gecici2->sonraki=gecici;//gecici2 değişkenimizden sonra gecici yani bizim eklediğimiz değişkeni göstersin dedik.
-	return head;// fonksiyonu cağırdıgımız yere head imizi döndürecek.
+	node *temp=new node();
+	temp-> data=7;
+	temp->next=NULL;
+	node *temp2=head;
+	while(temp2->next!=NULL)
+		temp2=temp2->next;
+	temp2->next=temp;
+	return head;
 }
-void goster(dugum *head) // Yapımızın tüm elemanlarını gösteren fonksiyon
+void prnt(node *head) // listeyi göster
 {
-	if(head==NULL)//yani hiçbir veri girilmemişse çalışak olan fonksiyon
+	if(head==NULL)
 	{
 		printf("Liste Boş\n");
 		return ;
 	}
-	dugum *gecici2=head;//gecici2 adlı değişkenimizi head eşitledik.Bunuda başlangıc sonuna kadar olan verileri bulmak için kullancagız.
-	while(gecici2 != NULL)//gecici2 adlı elemanızı baştan sona kadar döndürecek
+	node *temp2=head;
+	while(temp2 != NULL)
 	{
-		printf("%d\n",gecici2->veri);//yazdırma işlemi
-		gecici2=gecici2->sonraki;//gecisi2 elemanımız NULL oluncaya kadar bir sonrakini tanımlıyacak
+		printf("%d\n",temp2->data);
+		temp2=temp2->next;
 	}
 }
-int say(dugum *head) //listeyi kaç veri eklendiğinin sayısı veren fonksiyon
-{//algoritma olarak gösterme ile aynı sadece while döngüsün içine bir sayac ile saydırıcaz.
-	if(head==NULL)//listemiz boş ise
+int count (node *head) //listeyi say
+{
+	if(head==NULL)
 	{
 		printf("Liste Boş\n");
 		return 0;
 	}
-	dugum *gecici2 =head;//gecici2 adlı değişkenimizi head eşitledik.Bunuda başlangıc sonuna kadar olan verileri bulmak için kullancagız.
-	int counter = 0;//sayacımızı 0 başlatıyoruz
-	while(gecici2 != NULL)//gecici2 adlı elemanızı baştan sona kadar döndürecek
+	node *temp2 =head;
+	int counter = 0;
+	while(temp2 != NULL)
 	{
-		counter++;//sayacımız gecici2 null olana kadar 1 artacak
-		gecici2=gecici2->sonraki;//gecisi2 elemanımız NULL oluncaya kadar bir sonrakini tanımlıyacak
+		counter++;
+		temp2=temp2->next;
 	}
-	return counter;// fonksiyonu cağırdıgımız yere sayacımızı döndürecek.
+	return counter;
 }
-void rec_goster(dugum *head) //recursive fonksiyon=kendi kendini çagıran fonksiyondur.tüm elemanlarını gösteren fonksiyon
+void print_rec(node *head) //listeyi göster2
 {
-	if(head==NULL) return; //listemiz boş ise
+	if(head==NULL) return;
 	else
 	{
-		printf("%d\n",head->veri);//head imiz verisini göstercek
-		rec_goster(head->sonraki);//recursive fonksiyonumuz kendisi çagıracak ancak head imiz bir sonrakini
+		printf("%d\n",head->data);
+		print_rec(head->next);
 	}
 }
-dugum *sil(struct dugum *head,int key)//istenilen elemanımızı silen fonksiyon
+node *remove(struct node *head,int key)
 {
-	dugum *gecici=head;//gecici adlı değişkenimizi head eşitledik.Başlangıc elemanımzdan sona kadar gitmek için kullancagız
-	if(head->veri==key)//Eğer ilk elemanımız sileceğimiz elemana eşitse
+	struct node *temp=head;
+	if(head->data==key)
 	{
-		head=head->sonraki;//ilk elemanımızın bir sonraki artık ilk elemanız yani head imiz yap dedik
-		free(gecici);//head imizi silersek kaybolur bu yüzden gecici elemanımızı siliyoruz
+		head=head->next;
+		free(temp);
 	}
 	else
 	{
-		while(gecici->sonraki->veri!=key)//gecicimiz bir sonraki elemanız girdiğimiz elemana eşit oluncaya kadar
-			gecici=gecici->sonraki;//bir ileri götürecek
-		struct dugum *gecici2=gecici->sonraki;//giridiğimiz elemanı buldugumuzda bunu yeni bir gecici2 adlı değişkenimize atadık
-		gecici->sonraki=gecici->sonraki->sonraki;//sileceğimiz elemanda artık ondan sonra elemanı gösterecek
-		free(gecici2);//gecici2 değişkenimizi sildik
+		while(temp->next->data!=key)
+			temp=temp->next;
+		struct node *temp2=temp->next;
+		temp->next=temp->next->next;
+		free(temp2);
 	}
 	return head;
 }
-void ters(struct dugum *p)//Aynı resursive fonksiyon ile göstermenın tersten yazdırılış fonksiyonu
+void Reverse(struct node *p)
 {
-	if(p==NULL) return ;//listemiz boş ise
-	ters(p->sonraki);//recursive fonksiyonumuz kendisi çagıracak ancak p imiz bir sonrakini
-	printf("%d\n",p->veri);// p mizin verisini yazdırıcak
+	if(p==NULL) return ;
+	Reverse(p->next);
+	printf("%d\n",p->data);
 }
-int main()
+main()
 {
-dugum *head;
-	head =(dugum *)malloc(sizeof(dugum));
-	head->veri=2;
-	head->sonraki=NULL;
-	head->sonraki=(dugum *)malloc(sizeof(dugum));
-	head->sonraki->veri=5;
-	head->sonraki->sonraki=NULL;
+node *head;
+	head =new node();
+	head->data=1;
+	head->next=NULL;
+	head->next=new node();
+	head->next->data=3;
+	head->next->next=NULL;
 	printf("ilk liste\n");
-	goster(head);
-	printf("Eleman sayisi %d\n",say(head));
-	head=basaekle(head);
-	head=sonaekle(head);
-	head=sonaekle(head);
-	goster(head);
-	printf("Eleman sayisi %d\n",say(head));
-	sil(head,5);
-	sil(head,2);
-	sil(head,7);				
-	rec_goster(head);
-	printf("***************************\nTersten Yaz\n");
-	ters(head);
+	prnt(head);
+	printf("Eleman sayisi %d\n",count(head));
+	head=addhead(head);
+	head=addlast(head);
+	head=addlast(head);
+	prnt(head);
+	printf("Eleman sayisi %d\n",count(head));
+	remove(head,3);
+	remove(head,1);
+	remove(head,7);				
+	print_rec(head);
+	printf("***************************\n");
+	Reverse(head);
 	getch();
-
 }
