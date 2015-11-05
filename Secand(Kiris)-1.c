7d145be5696e7bd5b384f6db5910826d05cc6832
MEHMET AYHAN <mehtayn@hotmail.com>
2015-11-05 14:06:57 +0200

    Update Secand(Kiris)-1.c

--- Secand(Kiris)-1.c
+++ Secand(Kiris)-1.c
@@ -1,48 +1,43 @@
 // mehtayn.com
-//Secand(Kiris)-1
+// Secand(Kiris)-1 Yontemi
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <conio.h>
 
-
-const double pi=3.141592;
-
-double F(double p){
-    return cos(p)+2*sin(p)+p*p;
-}
+double f(double);
 
 int main()
 {
-    double p, p0, p1, error, tol;
-    tol=0.0001;
-
-    int n, i;
-
-    printf("Max Iterasyon sayisini giriniz\n");
-    scanf("%d",&n);
-
-    printf("Hata toleransini giriniz\n");
-    scanf("%lf",&tol);
-
-    printf("Baslangic noktasini giriniz\n");
-    scanf("%lf",&p0);
+    double p0 = 0.0;
+    double p1 = -0.01;
+    double p;
+    double errorRate = 0.0001;
+    int i = 2;
+    int N0 = 100;
 
 
-    for(i=0; i<n; i++){
-        p=p1-F(p1)*((p1-p0)/F(p1)-F(p0));
-        error=fabs(p-p0);
+    printf("%10s%20s%15s\n","Iteration","Approximate Root","Error Rate");
+    while(i < N0){
+        p = p1 - f(p1) * (p1 - p0) / (f(p1) - f(p0));
 
-        if(error<tol){
-            printf("%d. iterasyonda bulunan kok: %lf, ve hata: %lf bulunmustur", i, p, error);
-            getch();
-            return 0;
+        printf("%10d%20.3lf%15.4lf\n",i,p,fabs(p1-p0));
+
+        if(fabs(p-p1) < errorRate){
+            printf("Tamamdir.");
+            break;
         }
-        p0=p;
 
-        printf("%d. iterasyonda kok: %lf, hata: %lf\n", i, p, error);
+        p0 = p1;
+        p1 = p;
+        i++;
     }
-    printf("%d. iterasyon sonucu kok bulunamamistir\n", n);
 
     return 0;
+}
+
+double f(double x)
+{
+    double result = cos(x) + 2*sin(x) + pow(x,2);
+
+    return result;
 }
