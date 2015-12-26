0dc602c39bb74b2cbf65c34359077f818c3c7b9b
MEHMET AYHAN <mehtayn@hotmail.com>
2015-12-26 15:13:01 +0200

    Create SayisalintegralSimpson.c

---
+++ SayisalintegralSimpson.c
@@ -0,0 +1,30 @@
+// mehtayn.com
+// mehmetayhan.com.tr
+//Kod Örnekleri
+
+#include <stdio.h>
+#include <conio.h>
+double f(double c)
+{
+	return 1/c;
+}
+int main()
+{
+ 	double  h,b,a,toplam,x;
+ 	int n;
+	a=1;
+	b=2;
+	h=(b-a)/n;
+	toplam=f(a)+f(b);
+	for(int j=1;j<=n;j++)
+	{
+		x=a+j*h;
+		if(j%2==1)
+			toplam+=4*f(x);
+		else
+			toplam+=2*f(x);
+	}
+	toplam*=h/3;
+	printf("%lf",toplam);
+	getch();
+}
